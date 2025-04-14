b98eee47943197bcacbcc5a7bec4c91d68b9522d
Chorexxs <oswaldofonseca159@gmail.com>
2025-04-14 22:25:25 +0200

    función main

--- libwyag.py
+++ libwyag.py
@@ -13,3 +13,24 @@ import zlib
 argparse = argparse.ArgumentParser(
     description="Un seguidor de contenido de los Chinos")
 argsubparsers = argparse.add_subparsers(title="Comandos", dest="comando")
+
+
+def main(argv=sys.argv[1:]):
+    args = argparse.parse_args(argv)
+    match args.comando:
+        case "add": cmd_add(args)
+        case "cat-file": cmd_cat_file(args)
+        case "check-ignore": cmd_check_ignore(args)
+        case "checkout": cmd_checkout(args)
+        case "commit": cmd_commit(args)
+        case "hash-object": cmd_hash_object(args)
+        case "init": cmd_init(args)
+        case "log": cmd_log(args)
+        case "ls-files": cmd_ls_files(args)
+        case "ls-tree": cmd_ls_tree(args)
+        case "rev-parse": cmd_rev_parse(args)
+        case "rm": cmd_rm(args)
+        case "show-ref": cmd_show_ref(args)
+        case "status": cmd_status(args)
+        case "tag": cmd_tag(args)
+        case _: print(f"Comando no válido")
