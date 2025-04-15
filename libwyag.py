25fbd61e3e68d03fbf788ac71817cf3208b6277d
Chorexxs <oswaldofonseca159@gmail.com>
2025-04-15 22:56:22 +0200

    Trabajando en el Repositorio

--- libwyag.py
+++ libwyag.py
@@ -34,3 +34,39 @@ def main(argv=sys.argv[1:]):
         case "status": cmd_status(args)
         case "tag": cmd_tag(args)
         case _: print(f"Comando no válido")
+
+
+class GitRepository:
+    (object):
+    """Representa un repositorio de Git."""
+
+    worktree = None
+    gitdir = None
+    conf = None
+
+    def __init__(self, path, force=False):
+        self.worktree = path
+        self.gitdir = os.path.join(path, ".git")
+
+        if not (force or os.path.isdir(self.gitdir)):
+            raise Exception(f"No es un repositorio de Git válido: {path}")
+
+    # Lee el archivo de configuración en .git/config
+    self.conf = configparser.ConfigParser()
+    cf = repo_file(self, "config")
+
+    if cf and os.path.exists(cf):
+        self, conf.read([cf])
+    elif not force:
+        raise Exception("Archivo de configuración no encontrado")
+
+    if not force:
+        vers = int(self.conf.get("core", "repositoryformatversion"))
+        if vers != 0:
+            raise Exception(
+                f"Versión de formato de repositorio no soportada: {vers}")
+
+
+def repo_path(repo, *path):
+    """Calcula la ruta absoluta de un archivo en el repositorio."""
+    return os.path.join(repo.gitdir, *path)
