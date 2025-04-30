149ce901b0046ba56f5a94a37e6ae6c7622b6696
Chorexxs <oswaldofonseca159@gmail.com>
2025-04-30 10:57:49 +0200

    terminando la clase GitIgnore

--- libwyag.py
+++ libwyag.py
@@ -1028,3 +1028,76 @@ class GitIgnore(object):
     def __init__(self, absolute, scoped):
         self.absolute = absolute
         self.scoped = scoped
+
+
+def gitignore_read(repo):
+    ret = GitIgnore(absolute=list(), scoped=list())
+
+    repo_file = os.path.join(repo.gitdir, "info/exclude")
+    if os.path.exsists(repo_file):
+        with open(repo_file, "r") as f:
+            ret.absolute.append(gitignore_parse(f.readlines()))
+
+    if "XDG_CONFIG_HOME" in os.environ:
+        config_home = os.environ["XDG_CONFIG_HOME"]
+    else:
+        config_home = os.path.expanduser("~/.config")
+    global_file = os.path.join(config_home, "git/ignore")
+
+    if os.path.exists(global_file):
+        with open(global_file, "r") as f:
+            ret.absolute.append(gitignore_parse(f.readlines()))
+
+    index = index_read(repo)
+
+    for entry in index.entries:
+        if entry.name == ".gitignore" or entry.name.endswith("/.gitignore"):
+            dir_name = os.path.dirname(entry.name)
+            contents = object_read(repo, entry.sha)
+            lines = contents.blobdata.decode("utf-8").splitlines()
+            ret.scoped[dir_name] = gitignore_parse(lines)
+    return ret
+
+
+def check_ignore1(rules, path):
+    result = None
+    for (pattern, value) in rules:
+        if fnmatch(path, pattern):
+            result = value
+    return result
+
+
+def check_ignore_scoped(rules, path):
+    parent = os.path.dirname(path)
+    while True:
+        if parent in rules:
+            result = check_ignore1(rules[parent], path)
+            if result != None:
+                return result
+        if parent == "":
+            break
+        parent = os.path.dirname(parent)
+    return None
+
+
+def check_ignore_absolute(rules, path):
+    parent = os.path.dirname(path)
+    for ruleset in rules:
+        result = check_ignore1(ruleset, path)
+        if result != None:
+            return result
+    return False
+
+
+def check_ignore(rules, path):
+    if os.path.isabs(path):
+        raise Exception(
+            "Esta función requiere que el path sea relativo a la raíz del repositorio")
+
+    result = check_ignore_scoped(rules.scoped, path)
+    if result != None:
+        return result
+
+    return check_ignore_absolute(rules.absolute, path)
+
+# EL COMANDO status
